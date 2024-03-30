98dfa97ef75ad816c39fd6b22f249448451e54fc
ThitinanPang <65160217@go.buu.ac.th>
2024-03-30 19:16:23 +0700

    65160217
    65160217 ฐิตินันท์ วัชรมงคลกุล

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -13,3 +14,7 @@ Route::get('/add-user', function () {
 Route::get('/edit-user', function () {
 return view('editpage');
 });
+
+Route::get('user/delete/{id}', [UserController::class,'destroy']);
+
+Route::resource('user', UserController::class);
