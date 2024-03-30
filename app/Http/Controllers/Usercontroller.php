b1051c24f30785c8479c109c25985b71ab28c304
65160212 <65160212@go.buu.ac.th>
2024-03-30 22:35:46 +0700

    65160212
    65160212 นายคณิศร บุญประเสริฐ

--- app/Http/Controllers/Usercontroller.php
+++ app/Http/Controllers/Usercontroller.php
@@ -29,8 +29,6 @@ class UserController extends Controller {
             $validatedData['avatar'] = basename($avatarPath);
         }
 
-        $validatedData['password'] = bcrypt($validatedData['password']);
-
         $user = new User($validatedData);
         $user->save();
 
@@ -39,6 +37,7 @@ class UserController extends Controller {
 
     public function showAddPage() {
         $titles = Title::all();
+
         return view('addpage', compact('titles'));
     }
 
@@ -75,6 +74,7 @@ class UserController extends Controller {
 
         if ($request->isMethod('PUT')) {
             $user->update($validatedData);
+            
             return redirect()->route('home');
         }
     }
