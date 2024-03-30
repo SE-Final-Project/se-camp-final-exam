2d9a5ac33767797897ca2ae8b62b347e2ad94e3c
65160091Thanakit <152171630+65160091Thanakit@users.noreply.github.com>
2024-03-30 23:45:36 +0700

    65160091
    65160091 นายธนกฤต นนีสีลาด

--- resources/views/addpage.blade.php
+++ resources/views/addpage.blade.php
@@ -2,30 +2,101 @@
 
 @section('page_name', 'Add Users Data')
 @section('content')
+
+<head>
+    <title>Bootstrap Example</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css">
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.7.1/dist/jquery.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js"></script>
+  </head>
+<body>
+    
+
     <div class="card card-info">
         <div class="card-header">
             <h3 class="card-title">Add User Form</h3>
         </div>
         <!-- /.card-header -->
         <!-- form start -->
-        <form class="form-horizontal" action="{{ url('/') }}" method="post">
+        <form id="Myform" class="form-horizontal" action="{{ route('UserController.store') }}" method="post"  enctype="multipart/form-data">
             @csrf
             <div class="card-body">
                 <div class="form-group row">
-                    <label for="input01" class="col-sm-2 col-form-label">Example Input</label>
+                    <label  class="col-sm-2 col-form-label">Title</label>
                     <div class="col-sm-10">
-                        <input type="text" class="form-control" id="input01">
+                        <select class="form-control" name="title">
+                            <option value="คุณ">คุณ</option>
+                            <option value="นาย">นาย</option>
+                            <option value="นาง">นาง</option>
+                            <option value="นางสาว">นางสาว</option>
+                        </select>
+                    </div>
+                </div>
+                <div class="form-group row">
+                    <label  class="col-sm-2 col-form-label">Name</label>
+                    <div class="col-sm-10">
+                        <input type="text" class="form-control" id="name" name="name">
+                    </div>
+                </div>
+                <div class="form-group row">
+                    <label  class="col-sm-2 col-form-label">Email</label>
+                    <div class="col-sm-10">
+                        <input type="text" class="form-control" id="email" name="email">
+                    </div>
+                </div>
+                <div class="form-group row">
+                    <label  class="col-sm-2 col-form-label">Password</label>
+                    <div class="col-sm-10">
+                        <input type="text" class="form-control" id="password" name="password">
+                    </div>
+                </div>
+                <div class="form-group ">               
+                <div class="mb-3 row">
+                      <label  class="col-sm-2 col-form-label">Avatar</label>
+
+                    <div class="col-sm-10 custom-file ">
+                        <input type="file" class="custom-file-input" id="image" name="image" >
+                        <label class="custom-file-label" for="customFile">Choose file</label>                    
                     </div>
                 </div>
             </div>
+
+            </div>
+            
+            
             <!-- /.card-body -->
             <div class="card-footer">
-                <button type="submit" class="btn btn-info">Submit</button>
+                <button type="submit" value="Save" class="btn btn-info">Submit</button>
                 <a href="{{ url('/') }}" class="btn btn-default float-right">Cancel</a>
                 <button type="reset" class="btn btn-default float-right mr-2">Reset</button>
             </div>
             <!-- /.card-footer -->
         </form>
+
+        <script>
+            // Add the following code if you want the name of the file appear on select
+            $(".custom-file-input").on("change", function() {
+              var fileName = $(this).val().split("\\").pop();
+              $(this).siblings(".custom-file-label").addClass("selected").html(fileName);
+            });
+            </script>
+
+      
+
+        <script>
+            // Optional: Add event listener for form submission and prevent default behavior if validation fails
+            document.getElementById('Myform').addEventListener('submit', function(event) {
+              if (!this.checkValidity()) {
+                event.preventDefault();
+                alert('Please fill in all required fields!');
+              }
+            });
+          </script>
     </div>
+
+</body>
     <!-- /.card -->
 @endsection
--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -2,6 +2,10 @@
 
 @section('page_name', 'Users Data')
 @section('content')
+       <head>
+
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/2.1.2/sweetalert.min.js" integrity="sha512-AA1Bzp5Q0K1KanKKmvN/4d3IRKVlv9PYgwFPvm32nPO6QS8yH1HO7LbgB1pgiOxPtfeg5zEn2ba64MUcqJx6CA==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+        </head>
     <!-- CODE HERE -->
     <div class="float-right pb-4">
         <a href="{{ url('/add-user') }}" class="btn btn-success"> Add User </a>
@@ -10,25 +14,68 @@
         <thead>
             <tr>
                 <td width="35px">#</td>
+                <td>Title</td>
                 <td>name</td>
                 <td>email</td>
                 <td>avatar</td>
-                <td>Title</td>
                 <td width="150px">Tools</td>
             </tr>
         </thead>
         <tbody>
             <tr>
-                <td>1</td>
-                <td>name</td>
-                <td>email</td>
-                <td>avatar</td>
-                <td>Title</td>
-                <td>
-                    <a href="{{ url('/edit-user') }}" class="btn btn-warning">Edit</a>
-                    <button class="btn btn-danger">Delete</button>
-                </td>
-            </tr>
+
+                @foreach($items as $item)
+                <tr>
+                    <td>{{ $loop->iteration }}</td>
+                    <td>{{ $item->title }}</td>
+                    <td>{{ $item->name }}</td>
+                    <td>{{ $item->email }}</td>
+                    <td>
+                        <img src="{{ asset($item->image) }}" width= '50' height='50' class="img img-responsive" />
+                    </td>
+                    <td>
+                        <a href="{{ url('/edit-user',$item->id) }}" class="btn btn-warning">Edit</a>
+                        <a href="{{ url('/delete-user',$item->id) }}" class="btn btn-danger" onclick="confirmation(event)">Delete</a>    
+
+                        
+                    </td>
+                </tr>
+            @endforeach
+                
+            </tr> 
         </tbody>
     </table>
+    <script>
+        function confirmation(ev) {
+          ev.preventDefault();
+          var urlToRedirect = ev.currentTarget.getAttribute('href');  
+          console.log(urlToRedirect); 
+          swal({
+                title: "Are you sure?",
+                text: "You won't be able to revert this! ",
+                icon: "warning",
+                showCancelButton: true,
+                confirmButtonColor: "#DD6B55",
+                confirmButtonText: "Yes, delete it!",
+                showconfirmButton:true,
+                buttons: true
+
+
+          })
+          .then((willCancel) => {
+              if (willCancel) {
+
+                  window.location.href = urlToRedirect;
+              }  
+  
+  
+          });
+  
+          
+      }
+  </script>
+
+  
+<div>
+
 @endsection
