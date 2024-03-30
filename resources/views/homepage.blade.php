fd31459620c10770493e85d871fd0df2a33ba43a
Tanapat <98180053+TeEnInDy@users.noreply.github.com>
2024-03-30 20:44:13 +0700

    65160222

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -2,33 +2,50 @@
 
 @section('page_name', 'Users Data')
 @section('content')
-    <!-- CODE HERE -->
     <div class="float-right pb-4">
         <a href="{{ url('/add-user') }}" class="btn btn-success"> Add User </a>
     </div>
     <table class="table table-bordered">
         <thead>
             <tr>
-                <td width="35px">#</td>
-                <td>name</td>
-                <td>email</td>
-                <td>avatar</td>
-                <td>Title</td>
-                <td width="150px">Tools</td>
+                <th width="35px">#</th>
+                <th>Title</th>
+                <th>Name</th>
+                <th>Email</th>
+                <th>Avatar</th>
+                <th width="150px">Tools</th>
             </tr>
         </thead>
         <tbody>
-            <tr>
-                <td>1</td>
-                <td>name</td>
-                <td>email</td>
-                <td>avatar</td>
-                <td>Title</td>
-                <td>
-                    <a href="{{ url('/edit-user') }}" class="btn btn-warning">Edit</a>
-                    <button class="btn btn-danger">Delete</button>
-                </td>
-            </tr>
+            @foreach ($users as $user)
+                <tr>
+                    <td>{{ $user->id }}</td>
+                    <td>HI</td>
+                    {{-- <td>{{$user->title_id->tit_name}}</td> --}}
+                    <td>{{ $user->name }}</td>
+                    <td>{{ $user->email }}</td>
+                    <td>
+                        @if ($user->avatar != null)
+                            <img src="{{ asset('uploads/' . $user->avatar) }}" width="70px" height="70px" alt="image">
+                        @else
+                            ไม่มีรูปภาพ
+                        @endif
+                    </td>
+                    <td style="display:flex">
+                        <a href="{{ url('/edit-user/').'/'. $user->id}}" class="btn btn-warning">Edit</a>
+                        <form method="POST" action="{{ url('delete-user/' . $user->id) }}">
+                            @csrf
+                            @method('DELETE')
+                            <button onclick="myFunction2()" class="btn btn-danger" type="submit">Delete</button>
+                            <script>
+                                function myFunction2() {
+                                    alert("Delete Successfully!!!");
+                                }
+                            </script>
+                        </form>
+                    </td>
+                </tr>
+            @endforeach
         </tbody>
     </table>
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,11 @@
 <?php
-
+use App\Http\Controllers\C_User;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('homepage');
-});
+Route::get('/',[C_User::class,'index']);
+Route::get('/add-user',[C_User::class,'create']);
+Route::post('/add-user',[C_User::class,'store']);
+Route::get('edit-user/{id}',[C_User::class, 'edit']);
+Route::put('update-user/{id}',[C_User::class, 'update']); 
+Route::delete('delete-user/{id}',[C_User::class, 'destroy']);
 
-Route::get('/add-user', function () {
-    return view('addpage');
-});
-
-Route::get('/edit-user', function () {
-return view('editpage');
-});
