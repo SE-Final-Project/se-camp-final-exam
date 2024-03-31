5bd6423edb80c0c9e5d0914d229d750077b0a29a
WinnieTheBoom <win34086@gmail.com>
2024-03-31 23:55:02 +0700

    65160223
    นายธาวิน อุ้นวุ้น 65160223

--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,19 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\userControl;
 
-Route::get('/', function () {
-    return view('homepage');
-});
-
-Route::get('/add-user', function () {
-    return view('addpage');
-});
+Route::get('/edit-user/{id}', [userControl::class, 'editUser'])->name('edit.user');
+Route::put('/update-user/{id}', [userControl::class, 'updateUser'])->name('update.user');
 
 Route::get('/edit-user', function () {
-return view('editpage');
+    return view('editpage');
 });
+Route::get('/add-user', [App\Http\Controllers\userControl::class, 'showAddPage']);
+Route::post('/add-user', [App\Http\Controllers\userControl::class, 'addUser'])->name('add.user');
+Route::get('/add-user', function () {
+    $titles = App\Models\Title::all();
+    return view('addpage', compact('titles'));
+});
+
+Route::get('/', [userControl::class, 'id'])->name('home');
