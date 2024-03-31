74eb8a488f62e0bf3a8a7648713e8781c6177ad4
sakkarinapec <65160238@go.buu.ac.th>
2024-03-31 21:54:32 +0700

    65160238
    65160238 ศักรินทร์ บังเกิด

--- resources/views/editpage.blade.php
+++ resources/views/editpage.blade.php
@@ -8,22 +8,79 @@
         </div>
         <!-- /.card-header -->
         <!-- form start -->
-        <form class="form-horizontal" action="{{ url('/') }}" method="post">
+
+        <form class="form-horizontal" action="{{ url('/editdata') }}" method="post">
             @csrf
             <div class="card-body">
+
                 <div class="form-group row">
-                    <label for="input01" class="col-sm-2 col-form-label">Example Input</label>
+                    <label for="title" class="col-sm-2 col-form-label">Title</label>
                     <div class="col-sm-10">
-                        <input type="text" class="form-control" id="input01">
+                        {{-- <select class="form-control" id="title_id" name="title_id">
+                            @foreach ($titles as $title)
+                            <option value="{{$title->id}}">{{ $title->tit_name}}</option>
+                        @endforeach --}}
+                        <select class="form-control" id="title_id" name="title_id">
+                            <option>คุณ</option>
+                            <option>นาย</option>
+                            <option>นางสาว</option>
+                            <option>นาง</option>
+                            <option>อื่น ๆ </option>
+                        </select>
                     </div>
                 </div>
-            </div>
-            <!-- /.card-body -->
-            <div class="card-footer">
-                <button type="submit" class="btn btn-info">Submit</button>
-                <a href="{{ url('/') }}" class="btn btn-default float-right">Cancel</a>
-            </div>
-            <!-- /.card-footer -->
+
+                <div class="form-group row">
+                    <label for="name" class="col-sm-2 col-form-label">Name</label>
+                    <div class="col-sm-10">
+                        <input type="text" class="form-control" placeholder="กรอกชื่อ-นามสกุล" id="name"
+                            name="name">
+                    </div>
+                </div>
+
+                <div class="form-group row">
+                    <label for="email" class="col-sm-2 col-form-label">Email</label>
+                    <div class="col-sm-10">
+                        <input type="email" class="form-control <?php if($errors->has('email')){ ?>is-invalid<?php } ?>"
+                            placeholder="กรอกอีเมล" id="email">
+                    </div>
+                </div>
+
+                <div class="form-group row">
+                    <label for="avatar" class="col-sm-2 col-form-label">Avatar</label>
+                    <div class="col-sm-10">
+                        <div class="input-group mb-3">
+                            <div class="custom-file">
+                                <label class="custom-file-label" for="avatar">Choose file</label>
+                                <input type="file" class="custom-file-input form-control"
+                                    aria-describedby="inputGroupFileAddon01" id="avatar" name="avatar"
+                                    onchange="previewImage(this);">
+                            </div>
+                        </div>
+                        <img id="preview" src="#" alt="Preview" style="max-width: 100px; max-height: 100px;">
+                    </div>
+                </div>
+
+                <script>
+                    function previewImage(input) {
+                        var file = input.files[0];
+                        var reader = new FileReader();
+
+                        reader.onload = function(e) {
+                            $('#preview').attr('src', e.target.result);
+                        }
+
+                        reader.readAsDataURL(file);
+                    }
+                </script>
+
+                <!-- /.card-body -->
+                <div class="card-footer">
+                    <button type="submit" class="btn btn-info">Submit</button>
+                    <a href="{{ url('/') }}" class="btn btn-default float-right">Cancel</a>
+                    <button type="reset" class="btn btn-default float-right mr-2">Reset</button>
+                </div>
+                <!-- /.card-footer -->
         </form>
     </div>
     <!-- /.card -->
--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -10,25 +10,64 @@
         <thead>
             <tr>
                 <td width="35px">#</td>
-                <td>name</td>
-                <td>email</td>
-                <td>avatar</td>
+
                 <td>Title</td>
+                <td>Name</td>
+                <td>Email</td>
+                <td>Avatar</td>
+
                 <td width="150px">Tools</td>
             </tr>
         </thead>
         <tbody>
-            <tr>
-                <td>1</td>
-                <td>name</td>
-                <td>email</td>
-                <td>avatar</td>
-                <td>Title</td>
-                <td>
-                    <a href="{{ url('/edit-user') }}" class="btn btn-warning">Edit</a>
-                    <button class="btn btn-danger">Delete</button>
-                </td>
-            </tr>
+
+            @foreach ($users as $index => $item)
+                <tr>
+                    <td>{{ $index + 1 }}</td>
+                    <td>{{ $item->title->tit_name }}</td>
+
+                    <td>{{ $item->name }}</td>
+                    <td>{{ $item->email }}</td>
+                    <td>ไม่มีรูป</td>
+                    {{-- <td>{{ $item->avatar }}</td> --}}
+                    {{-- @if ($users->item$item->avatar!= null)
+                        <img src="{{ asset('uploads/' . $user->avatar) }}" width="70px" height="70px" alt="image">
+                    @else
+                        ไม่มีรูป
+                    @endif --}}
+
+                    </td>
+                    <td>
+                        <a href="{{ url('/edit-user/{id}') }}" class="btn btn-warning">Edit</a>
+                        <button class="btn btn-danger">Delete</button>
+                    </td>
+                </tr>
+            @endforeach
+
         </tbody>
     </table>
 @endsection
+@section('js')
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+
+    <script type="text/javascript">
+        $('.show_confirm').click(function(event) {
+          var form =  $(this).closest("form");
+          event.preventDefault();
+          Swal.fire({
+        title: "Are you sure?",
+        text: "You won't be able to revert this!",
+        icon: "warning",
+        showCancelButton: true,
+        confirmButtonColor: "#3085d6",
+        cancelButtonColor: "#d33",
+        confirmButtonText: "Yes, delete it!"
+      }).then((result) => {
+
+        if (result.isConfirmed) {
+            form.submit();
+        }
+      });
+    });
+    </script>
