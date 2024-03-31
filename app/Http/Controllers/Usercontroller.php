dd5e3fbddce5c89dfb248ff1736f226a4465b086
Boya0828 <kidohagai@gmail.com>
2024-03-31 15:36:56 +0700

    Update Usercontroller.php

--- app/Http/Controllers/Usercontroller.php
+++ app/Http/Controllers/Usercontroller.php
@@ -12,42 +12,38 @@ use function Laravel\Prompts\alert;
 
 class UserController extends Controller
 {
+
+    //show hompage 65160360 sirichai paenpiriya
     public function showHomePage()
     {
-        $users = User::all();
-        return view('homepage', ['users' => $users]);
+        $users ['users']= User::orderBy('id','asc');
+        return view('homepage', $users);
     }
-
+    //show add page 65160360 sirichai paenpiriya
     public function Create()
     {
-        $titles = Title::orderBy('id')->get();
-
-        return view('addpage', ['titles' => $titles]);
+        $titles['titles'] = Title::orderBy('id')->get();
+        return view('addpage',  $titles);
     }
-
+    //show add edit page 65160360 sirichai paenpiriya
     public function showEdit($id)
     {
         $user = User::find($id);
-        $titles = Title::all();
+        $titles = Title::orderBy('id','asc');
         return view('editpage', compact('user', 'titles'));
     }
 
-
+    //store data 65160360 sirichai paenpiriya
     public function store(Request $request)
     {
 
         $validator = Validator::make($request->all(), [
             'title_id' => 'required',
             'name' => 'required',
-            'email' => 'required',
+            'email' => 'required',//can 1 mail unique
             'password' => 'required',
             'avatar' => 'image',
         ]);
-        $title_name = $request->input('title');
-        $title = Title::where('tit_name', $title_name)->first();
-        if ($title) {
-            $user->title_id =  $title->id;
-        }
         $user = new User();
         $user->title_id = $request->title_id;
         $user->name = $request->name;
@@ -64,6 +60,7 @@ class UserController extends Controller
         return redirect()->route('homepage')->with('success', 'User has been added successfully!');
     }
 
+    //update data 65160360 sirichai paenpiriya
     public function updateUser(Request $request, $id){
         $validator = Validator::make($request->all(), [
             'title_id' => 'required',
@@ -79,6 +76,7 @@ class UserController extends Controller
         $user->name = $request->name;
         $user->email = $request->email;
         $user->password = bcrypt($request->password);
+        //เช็คไฟล์ภาพ
         if ($request->hasFile('avatar')) {
             $fileName = time().$request->file('avatar')->getClientOriginalName();
             $avatarPath = $request->file('avatar')->storeAs('avatars',$fileName,'public');
@@ -89,13 +87,11 @@ class UserController extends Controller
         $user->save();
         return redirect()->route('homepage')->with('success','company has been updated successfully');
     }
+    //delete data 65160360 sirichai paenpiriya
 
-    public function deleteUser($id)
+    public function deleteUser(User $users)
     {
-    $user = User::findOrFail($id);
-
     $user->delete();
-
     return redirect()->route('homepage')->with('success', 'User has been deleted successfully.');
     }
 
